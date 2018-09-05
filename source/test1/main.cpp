c10dd8856fb77da2dfdc2d90f7611d8af87217e9
Ilya Smirnov <ilya2smirnov@yandex.ru>
2018-09-05 04:05:33 +0300

    add int c

--- source/test1/main.cpp
+++ source/test1/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char* argv[]) {
     int a = 3 + 4;
     int b = 7 - 8;
+    int c = 3 - 1;
     std::cout << "Main project" << std::endl;
+    std::cout << "a + b + c = " << a+b+c << std::endl;
     return 0;
 }
