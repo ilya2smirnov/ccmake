b271cf2bf7b60c3370e9887cf08492fc2786cc02
Ilya Smirnov <ilya2smirnov@yandex.ru>
2018-09-05 04:12:38 +0300

    multiply instead of summarize

--- source/test1/main.cpp
+++ source/test1/main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char* argv[]) {
     int b = 7 - 8;
     int c = 3 - 1;
     std::cout << "Main project" << std::endl;
-    std::cout << "a + b + c = " << a+b+c << std::endl;
+    std::cout << "a * b * c = " << a*b*c << std::endl;
     return 0;
 }
